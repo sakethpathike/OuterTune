5365fcb9da638155d913da3690eb320c759ef1e1
Zion Huang <zionhuang1107@gmail.com>
2024-08-30 14:46:59 +0800

    multiselect: play next in multi-selection menu

--- app/src/main/java/com/dd3boh/outertune/ui/menu/SelectionSongsMenu.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/menu/SelectionSongsMenu.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.PlaylistAdd
+import androidx.compose.material.icons.automirrored.rounded.PlaylistPlay
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
@@ -34,6 +35,7 @@ import com.dd3boh.outertune.LocalDatabase
 import com.dd3boh.outertune.LocalDownloadUtil
 import com.dd3boh.outertune.LocalPlayerConnection
 import com.dd3boh.outertune.R
+import com.dd3boh.outertune.extensions.toMediaItem
 import com.dd3boh.outertune.models.MediaMetadata
 import com.dd3boh.outertune.playback.ExoDownloadService
 import com.dd3boh.outertune.playback.PlayerConnection.Companion.queueBoard
@@ -183,6 +185,15 @@ fun SelectionMediaMetadataMenu(
                     items = selection
                 )
             )
+            clearAction()
+        }
+
+        GridMenuItem(
+            icon = Icons.AutoMirrored.Rounded.PlaylistPlay,
+            title = R.string.play_next,
+        ) {
+            onDismiss()
+            playerConnection.playNext(selection.map { it.toMediaItem() })
             clearAction()
         }
 
